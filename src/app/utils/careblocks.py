f8f8393f2e9aaf8539c56108d6845e7cab2928d1
Mohamed Farouk <admiralmoha@gmail.com>
2019-06-29 09:00:22 +0200

    [ADD] CareBlocks: Utility class for interacting with blockchain

--- src/app/utils/careblocks.py
+++ src/app/utils/careblocks.py
@@ -6,10 +6,10 @@ from common.config import ETH_NODE_URI, CONTRACTS_PATH
 import os
 import json
 from web3 import Web3
-from solc import compile_source
 
 
-# class
+# Care Block Utility Class
+
 class CareBlocks:
 
     w3 = None
@@ -22,7 +22,10 @@ class CareBlocks:
             self.w3 = Web3(Web3.HTTPProvider(
                 ETH_NODE_URI, request_kwargs={'timeout': 30}))
             if self.isConnected():
-                print("Connected to blockchain")
+                print("Connected to blockchain!")
+                print("Started..")
+                print("Welcome to CareBlocks :D")
+
             else:
                 print("W3 couldn't connect to blockchain!")
         
@@ -31,6 +34,7 @@ class CareBlocks:
 
     # check connection
     def isConnected(self):
+        print("We have a valid connection to blockchain")
         return self.w3.isConnected()
 
     # get contract path
@@ -68,18 +72,45 @@ class CareBlocks:
         return self.contracts
     
 
-    # deploy contract
+    # deploy smart contract to chain
     def deploy_contract(self, name):
-        contract = contracts.get(name, {})
+        contract = self.contracts.get(name, {})
+        w3 = self.w3
 
         if len(contract) == 0:
             contract = self.get_contract_json(name)
-        
+            if contract:
+                self.contracts[name] = contract
+
+                #instantiate contract
+                contract_eth_instance = w3.eth.contract(
+                    abi=contract['abi'],
+                    bytecode=contract['bytecode'])
+
+                # set account that will deploy the smart contract
+                w3.eth.defaultAccount = w3.eth.accounts[0]
+
+                # deploy contract
+                tx_hash = contract_eth_instance.constructor().transact()
+                print("Hash: ", tx_hash)
+                # get transaction receipt which has address
+                print("Bout to send transaction")
+                tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash, timeout=120)
+                contract_address = tx_receipt['contractAddress']
+                print("contract address:", contract_address)
+                print("Contract deployed!")
+            else:
+                print("Failed to delpy contract.\nMake sure contract is compiled with truffle!")
 
         
     
-    # get data from contract#
+    # get data from contract
     # set data on contract
 
 # if __name__ == '__main__':
     # cb = CareBlocks()
+    # print(cb.get_contract_json('CareBlock'))
+    # cb.isConnected()
+    # cb.deploy_contract('CareBlock')
+
+# TODO add full comments
