4d9bf3cc129c663d448e08cce9f6866327a7b3e5
Ahmed Mohamed <amshrbo@gmail.com>
2019-07-03 02:01:15 +0200

    [UPDATE] ipfs.py: adding new medical record for an existing patient

--- src/app/utils/ipfs.py
+++ src/app/utils/ipfs.py
@@ -1,5 +1,5 @@
 import ipfshttpclient as ipfs
-import os, json
+import os, json, time, shutil
 
 class IpfsEmr:
     def __init__(self, host, port):
@@ -8,32 +8,15 @@ class IpfsEmr:
 
         except ipfs.exceptions.ConnectionError as e:
             print(f'Wollah, Not able to establish a connection given this error {e}.')
-        
-
-    def add_file(self, file_path):
-        if os.path.isfile(file_path):
-            res = self._client.add(file_path)
-            file_hash = res['Hash']
-            print(f'File has been added with an address {file_hash}')
-            return file_hash
-
-        else:
-            print('The given path is not valid')
-            exit(1)
+  
 
 
-    def get_file(self, file_hash):
+    def push_patient_folder(self, folder_path):
         """
-            taking the file hash and retrive the file from ipfs
-            return the content of the given file as a string
+        Pushing an entire folder to ipfs and returning the hash of the
+        parent folder to be stored in the blockchain
         """
-        content = self._client.cat(file_hash)
-        content_str = content.decode("utf-8")
 
-        return content_str
-
-        
-    def add_folder(self, folder_path):
         if os.path.isdir(folder_path):
             res = self._client.add(folder_path, recursive=True)
             folder_hash = res[-1]['Hash']
@@ -45,7 +28,35 @@ class IpfsEmr:
             exit(1)
 
 
-    def get_folder(self, folder_hash):
+
+    def get_patient_folder(self, folder_hash):
+        """
+        Downloading the entire folder for a given ptient to add the new
+        data (files) to it
+        """
+
+        self._client.get(folder_hash)
+
+
+
+    def create_folder_structure(self, patient_name):
+        """
+        Creating the folder structure for every NEW_ADDED patient
+        to the blockchain, and creates initial two folders
+        (lab_results, medical_records)
+        """
+
+        lab_results = f'./{patient_name}/lab_results'
+        medical_records = f'./{patient_name}/medical_records'
+
+        os.makedirs(lab_results, exist_ok=True)
+        os.makedirs(medical_records, exist_ok=True)
+
+        return self.push_patient_folder(f'./{patient_name}')
+
+
+
+    def cat_folder_content(self, folder_hash):
         """
         Prints the content of the existing files in the given folder
         """
@@ -59,36 +70,63 @@ class IpfsEmr:
             file_name = file_data['Name']
 
             content = self._client.cat(file_hash).decode('utf-8')
-            print(f'The {file_name} contains: \n{content}')
-    
+            print(f'The {file_name} contains: \n{content}')    
 
 
-    def create_folder_structure(self, patient_name):
+
+    def add_medical_record(self, folder_hash, data):
         """
-        Creating the folder structure for every NEW_ADDED patient
-        to the blockchain, and creates initial files with them to 
-        store meta data about our two folders (lab_results, medical_records)
+        Adding new medical record for a given patient taking a json file as the data
+        which will contain two fileds the first one the lab results and the second will
+        be the medical record
+        json_file{
+            'lr': {
+                'file_ext_1': 'ext',
+                'encrypted_data_1': 'data',
+                'file_ext_2': 'ext',
+                'data': 'encrypted_data'
+            },
+            'mr': 'encrypted_json_file'
+        }
         """
 
-        lab_results = f'./{patient_name}/lab_results'
-        medical_records = f'./{patient_name}/medical_records'
+        self.get_patient_folder(folder_hash)
+        
+        try:
+            data = json.loads(data)
+        except Exception as exc:
+            print(f'Can not parse the file given the exception: \n {exc}')
+        
+        if data['mr'] is None:
+            print("Null value for the medical record in the given data")
+        else:
+            medical_record_data = data['mr']
 
-        meta_data_lr = f'./{patient_name}/lab_results/meta_data_lr.json'
-        meta_data_mr = f'./{patient_name}/medical_records/meta_data_mr.json'
+            fname_as_time_stam = time.strftime("%Y%m%d-%H%M%S")
+            medical_record_file_path = f'{folder_hash}/medical_records/{fname_as_time_stam}'
+
+            try:
+                with open(medical_record_file_path, 'x') as mr_file:
+                    mr_file.write(medical_record_data)
+            except Exception as exc:
+                print(f'can not create and write to the new medical record {exc}')
 
 
-        os.makedirs(lab_results, exist_ok=True)
-        os.makedirs(medical_records, exist_ok=True)
+        # TODO writing more than one file with their extension
+        if data['lr'] is None:
+            print('Null value for the lr in the given data')
+        else:
+            lab_result = data['lr']
+            print(lab_result)
 
-        # creating initial files as meta data for every folder to keep track of the folders
-        data = {'current_num': 0}
+        new_folder_hash = self.push_patient_folder(f'./{folder_hash}')
 
-        create_meta_data_files(path=meta_data_lr, data=data)
-        create_meta_data_files(path=meta_data_mr, data=data)
+        # TODO removing the old folder and unpin it from ipfs
+        # print('remoing the old directory')
+        # shutil.rmtree(f'./{folder_hash}')
 
-        # Pushing the folder to the blockchain
 
-        return self.add_folder(f'./{patient_name}')
+        return new_folder_hash
 
 
 
@@ -101,16 +139,22 @@ class IpfsEmr:
 
 
 
-def create_meta_data_files(path, data):
-    with open(path, 'w', encoding='utf-8') as outfile:
-        json.dump(data, outfile, ensure_ascii=False, indent=2)
-
-
-
 if __name__ == '__main__':
     ipfs = IpfsEmr('127.0.0.1', '5001')
     
-    new_user = 'Ahmed'
+    new_user = 'Robin'
     new_user_Hash = ipfs.create_folder_structure(new_user)
 
-    
+    testing_data = {
+        'lr': {
+            'file_ext': 'png',
+            'data': '64byte formting data'
+        },
+        'mr': 'an encrypted content'
+    }
+    testing_str = json.dumps(testing_data)
+
+    updated_hash = ipfs.add_medical_record(new_user_Hash, testing_str)
+    print(updated_hash)
+
+    ipfs.get_patient_folder(updated_hash)
