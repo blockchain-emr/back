70c0d8f56a13bfd06b8f193d33d528dad87b7cdc
Ahmed Mohamed <amshrbo@gmail.com>
2019-07-04 06:09:47 +0200

    [UPDATE] ipfs.py: retrieving all the medical records content for a given patient

--- src/app/utils/ipfs.py
+++ src/app/utils/ipfs.py
@@ -1,6 +1,7 @@
 import ipfshttpclient as ipfs
 import os, json, time, shutil
 
+
 class IpfsEmr:
     def __init__(self, host, port):
         try:
@@ -8,7 +9,24 @@ class IpfsEmr:
 
         except ipfs.exceptions.ConnectionError as e:
             print(f'Wollah, Not able to establish a connection given this error {e}.')
+            exit(1)
+
+
+
+    def get_file_content(self, file_hash):
+        """
+        Getting the content for a given file hash as a string
+        """
+        data = self._client.cat(file_hash).decode('utf-8')
+        return data
   
+
+
+    def ls_folder_content(self, folder_hash):
+        folder_data = self._client.ls(folder_hash)
+        folder_content = folder_data['Objects'][0]['Links']
+        return folder_content
+
 
 
     def push_patient_folder(self, folder_path):
@@ -29,12 +47,7 @@ class IpfsEmr:
 
 
 
-    def get_patient_folder(self, folder_hash):
-        """
-        Downloading the entire folder for a given ptient to add the new
-        data (files) to it
-        """
-
+    def pull_patient_folder(self, folder_hash):
         self._client.get(folder_hash)
 
 
@@ -52,32 +65,20 @@ class IpfsEmr:
         os.makedirs(lab_results, exist_ok=True)
         os.makedirs(medical_records, exist_ok=True)
 
-        return self.push_patient_folder(f'./{patient_name}')
+        folder_hash = self.push_patient_folder(f'./{patient_name}')
 
+        # removing folder after pushing it
+        print('removing the old directory')
+        shutil.rmtree(f'./{patient_name}')
 
-
-    def cat_folder_content(self, folder_hash):
-        """
-        Prints the content of the existing files in the given folder
-        """
-        # first list all the files that exist in this folder
-        result = self._client.ls(folder_hash)
-        files_data = result['Objects'][0]['Links']
-
-        for file_data in files_data:
-            # getting the fiels hash and name for every file and just print them for now
-            file_hash = file_data['Hash']
-            file_name = file_data['Name']
-
-            content = self._client.cat(file_hash).decode('utf-8')
-            print(f'The {file_name} contains: \n{content}')    
+        return folder_hash
 
 
 
     def add_medical_record(self, folder_hash, data):
         """
         Adding new medical record for a given patient taking a json file as the data
-        which will contain two fileds the first one the lab results and the second will
+        which will contain two fields the first one the lab results and the second will
         be the medical record
         json_file{
             'lr': {
@@ -90,7 +91,7 @@ class IpfsEmr:
         }
         """
 
-        self.get_patient_folder(folder_hash)
+        self.pull_patient_folder(folder_hash)
         
         try:
             data = json.loads(data)
@@ -102,14 +103,15 @@ class IpfsEmr:
         else:
             medical_record_data = data['mr']
 
-            fname_as_time_stam = time.strftime("%Y%m%d-%H%M%S")
+            fname_as_time_stam = time.strftime("%Y%m%d%H%M%S")
             medical_record_file_path = f'{folder_hash}/medical_records/{fname_as_time_stam}'
 
             try:
                 with open(medical_record_file_path, 'x') as mr_file:
+                    print('writing to the local file')
                     mr_file.write(medical_record_data)
             except Exception as exc:
-                print(f'can not create and write to the new medical record {exc}')
+                print(f'Can not create and write to the new medical record {exc}')
 
 
         # TODO writing more than one file with their extension
@@ -121,12 +123,63 @@ class IpfsEmr:
 
         new_folder_hash = self.push_patient_folder(f'./{folder_hash}')
 
-        # TODO removing the old folder and unpin it from ipfs
-        # print('remoing the old directory')
-        # shutil.rmtree(f'./{folder_hash}')
-
-
         return new_folder_hash
+
+
+
+    def get_patient_mrs(self, folder_hash):
+        """
+        Getting the content of the MRs for a given patient from ipfs then
+        concatinate all those MR for returning it to the user as one json file
+        """
+        # Getting the MR folder hash
+        nested_folders_data = self.ls_folder_content(folder_hash)
+
+        mr_folder_hash = ''
+        for folder in nested_folders_data:
+            if folder['Name'] == 'medical_records':
+                mr_folder_hash = folder['Hash']
+                print(f'Getting medical record folder Hash:{mr_folder_hash}')
+                break
+        
+        #Getting all the medical record given the mr_folder_hash
+        if mr_folder_hash:
+            files_data = self.ls_folder_content(mr_folder_hash)
+        else:
+            print('Can not get the medical record folder hash')
+            exit(1)
+
+        # getting the files content and compine them together
+        all_mr_data = []
+        if files_data:
+            for file_info in files_data:
+                file_content = self.get_file_content(file_info['Hash'])
+                data = {'Name': file_info['Name'], 'Content': file_content}
+                all_mr_data.append(data)
+        else:
+            print('Can not get the medical record files data')
+            exit(1)
+        
+        # ordering the list
+        all_mr_data_sorted = sorted(all_mr_data, key=lambda k: k['Name'])
+        
+        return json.dumps(all_mr_data_sorted)
+
+
+
+    def get_patient_data(self, folder_hash):
+        """
+        Getting all the data for a given patient These data 
+        Includes all his medical records and all his lab results
+        """
+       
+        pass
+
+
+
+    def get_patient_lr(self, folder_hash):
+        #TODO looping over all the lr files and compine them into one file
+        pass
 
 
 
@@ -140,21 +193,35 @@ class IpfsEmr:
 
 
 if __name__ == '__main__':
-    ipfs = IpfsEmr('127.0.0.1', '5001')
-    
-    new_user = 'Robin'
-    new_user_Hash = ipfs.create_folder_structure(new_user)
 
-    testing_data = {
+    ipfsemr = IpfsEmr('127.0.0.1', '5001')
+
+    # creating a user in the blockchain
+    robin = 'robin'
+    robin_hash = ipfsemr.create_folder_structure(robin)
+
+    # pushing a medical record to ipfs
+    first_mr = {
         'lr': {
             'file_ext': 'png',
             'data': '64byte formting data'
         },
-        'mr': 'an encrypted content'
+        'mr': 'an encrypted content first test'
     }
-    testing_str = json.dumps(testing_data)
+    robin_new_hash_1 = ipfsemr.add_medical_record(robin_hash, json.dumps(first_mr))
+    
+    #pushing another medical record to ipfs
+    second_mr = {
+        'lr': {
+            'file_ext': 'png',
+            'data': '64byte formting data'
+        },
+        'mr': 'an encrypted content second test'
+    }
+    robin_new_hash_2 = ipfsemr.add_medical_record(robin_new_hash_1, json.dumps(second_mr))
 
-    updated_hash = ipfs.add_medical_record(new_user_Hash, testing_str)
-    print(updated_hash)
 
-    ipfs.get_patient_folder(updated_hash)
+    # Getting all the medical records for robin
+    robin_mrs_content = ipfsemr.get_patient_mrs(robin_new_hash_2)
+    print(robin_mrs_content)
+
