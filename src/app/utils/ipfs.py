827af07e4ae232faf3afbae678cf774c55cdfe14
Ahmed Mohamed <amshrbo@gmail.com>
2019-07-08 01:07:16 +0200

    [UPDATE] ipfs.py: EDITS AFTER THE REDESIGN: add_patient, get_profile, edit_profile

--- src/app/utils/ipfs.py
+++ src/app/utils/ipfs.py
@@ -6,9 +6,34 @@ class IpfsEmr:
     def __init__(self, host, port):
         try:
             self._client = ipfs.connect(f'/ip4/{host}/tcp/{port}/http', session=True)
+            print(f'\n...... Connection Established with ipfs host: {host}, and port: {port} ......\n')
 
         except ipfs.exceptions.ConnectionError as e:
-            print(f'Wollah, Not able to establish a connection given this error {e}.')
+            print(f'Wollah, Not able to establish a connection given the below ERROR \n{e}.')
+            exit(1)
+
+
+
+    def get_json_file(self, file_hash):
+        data = self._client.get_json(file_hash)
+
+        # make sure the comming data in dict format
+        if not isinstance(data, dict):
+            data = json.loads(data)
+            return data
+        else:
+            return data
+        
+
+
+    def push_json_file(self, json_file):
+        file_hash = self._client.add_json(json_file)
+
+        if file_hash:
+            return file_hash
+
+        else:
+            print(f'Can not push the json file')
             exit(1)
 
 
@@ -19,9 +44,52 @@ class IpfsEmr:
         """
         data = self._client.cat(file_hash).decode('utf-8')
         return data
-  
 
 
+    
+    def add_new_patient(self, patient_profile_data):
+        """ creating the json file for a new added patient returning the file hash """
+
+        if not isinstance(patient_profile_data, dict):
+            print(f'Converting the patient_pro_data into json object')
+            patient_profile_data = json.loads(patient_profile_data)
+        
+        patient_data = {
+            'profile_data': patient_profile_data,
+            'appointments': {},
+            'chronic': '',
+            'lab_results': ''
+        }
+
+        patient_hash = self.push_json_file(patient_data)
+        print(f'Successfully added a new patient with file hash: {patient_hash}')
+
+        return patient_hash
+
+
+
+    def get_patient_profile(self, patient_hash):
+        """ retreiving the main patient json file and gets the profile data from it"""
+        patient_data = self.get_json_file(patient_hash)
+        profile = patient_data['profile_data']
+
+        return profile
+        
+
+
+    def edit_patient_profile(self, patient_hash, new_profile):
+        patient_data = self.get_json_file(patient_hash)
+        patient_data['profile_data'] = new_profile
+
+        print(f'The new edited patient data is {patient_data["profile_data"]}')
+
+        new_patient_hash = self.push_json_file(patient_data)
+        print(f'Successfully edited the patient profile')
+
+        return new_patient_hash
+
+
+    # README -> DEPRECATION FROM HERE TILL THE END
     def ls_folder_content(self, folder_hash):
         folder_data = self._client.ls(folder_hash)
         folder_content = folder_data['Objects'][0]['Links']
@@ -188,9 +256,7 @@ class IpfsEmr:
 
 
     def close(self):
-        """
-            Closing the established connection
-        """
+        print('\n...... Closing Connection :) ......\n')
         self._client.close()
 
 
@@ -200,32 +266,28 @@ if __name__ == '__main__':
 
     ipfsemr = IpfsEmr('127.0.0.1', '5001')
 
-    # creating a user in the blockchain
-    robin = 'HelloThere'
-    robin_hash = ipfsemr.create_folder_structure(robin)
-
-    # pushing a medical record to ipfs
-    first_mr = {
-        'lr': {
-            'file_ext': 'png',
-            'data': '64byte formting data'
-        },
-        'mr': 'an encrypted content first test'
+    # dummy data comming with add_patient request
+    patient_prof = {
+        'name': 'Nico Robin',
+        'mail': 'Robin@mail.com',
+        'phone': '0102222255',
+        'address': '0xsdfsf4sdf54454',
     }
-    robin_new_hash_1 = ipfsemr.add_medical_record(robin_hash, json.dumps(first_mr))
-    
-    #pushing another medical record to ipfs
-    second_mr = {
-        'lr': {
-            'file_ext': 'png',
-            'data': '64byte formting data'
-        },
-        'mr': 'an encrypted content second test'
+
+    # adding the patient
+    robin_hash = ipfsemr.add_new_patient(patient_prof)
+    print(f'The new added patient: {ipfsemr.get_patient_profile(robin_hash)}')
+
+    # editing the profile info
+    patient_prof_edit = {
+        'name': 'Robin',
+        'mail': 'awesomerobin@mail.com',
+        'phone': '0102222255',
+        'address': '0xsdfsf4sdf54454',
     }
-    robin_new_hash_2 = ipfsemr.add_medical_record(robin_new_hash_1, json.dumps(second_mr))
 
+    new_robin_hash = ipfsemr.edit_patient_profile(robin_hash, patient_prof_edit)
+    print(f'Get the new patient profile: {ipfsemr.get_patient_profile(new_robin_hash)}')
 
-    # Getting all the medical records for robin
-    robin_mrs_content = ipfsemr.get_patient_mrs(robin_new_hash_2)
-    print(robin_mrs_content)
+    ipfsemr.close()
 
