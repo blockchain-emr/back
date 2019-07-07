263afb52139046810add8cdc1f78ff401eeb33e1
hossam <new.hossam0@gmail.com>
2019-07-07 23:04:53 +0200

    [ADD] registeration endpoint

--- src/app/api/authentication.py
+++ src/app/api/authentication.py
@@ -1,6 +1,7 @@
-import sys, json
+import sys, json,threading, time
 sys.path.append("..")
 from common.config import *
+from utils import general as gutils
 from utils import ethereum as eth
 
 
@@ -32,3 +33,28 @@ def auth():
     refresh_token = create_refresh_token(identity=user_identity,expires_delta = refresh_expire)
 
     return jsonify(access_token=access_token,refresh_token=refresh_token), 200
+
+
+@app.route('/register',methods=['POST'])
+@swag_from('../docs/swagger/accounts_management/register.yml')
+def register():
+    if not request.is_json:
+        return jsonify(msg="Unacceptable data format."), 406
+
+    first_name = request.json.get("first_name",None)
+    last_name = request.json.get("last_name",None)
+    email = request.json.get("email",None)
+    gender =request.json.get("gender",None)
+    phone_number = request.json.get("phone_number",None)
+    password = request.json.get("password",None)
+
+    eth_address = eth.create_account(password)
+    if not eth_address:
+        return jsonify(msg="Error happened, not created."), 500
+    else:
+        print("Creating thread ...")
+        thread = threading.Thread(target=gutils.store_user_data, args=(first_name,last_name,email,gender,phone_number))
+        thread.daemon = True
+        thread.start()
+
+    return jsonify(address=eth_address), 201
