b606574f989d7cc592ae2c47c8669672084c03e8
Mohamed Farouk <admiralmoha@gmail.com>
2019-07-13 15:50:56 +0200

    [REMOVE] CareBlock: patient name from chain

--- src/app/truffle/contracts/CareBlock.sol
+++ src/app/truffle/contracts/CareBlock.sol
@@ -5,7 +5,6 @@ contract CareBlock {
 
 	struct Patient {
 		uint id;
-		string name;
 		string IPFSCareBlock; // address of patient.json file which contains references to all their data
 		bool verified;
 	}
@@ -16,14 +15,13 @@ contract CareBlock {
 		// this is just used for testing
 		addPatient(
 			0xAd663308B4c65C23bb01C08a002331D1d8878C71,
-			"James T. Kirk",
 			'QmZtdeqehYHM6ahSsgRJdS7kMR1FMBuU9Dyup9CnSfEC3U'
 		);
 	}
 
-	function addPatient(address _patientAddress, string memory name, string memory _ipfsAddress) public {
+	function addPatient(address _patientAddress, string memory _ipfsAddress) public {
 		patientCount++;
-		patients[_patientAddress] = Patient(patientCount, name, _ipfsAddress, false);
+		patients[_patientAddress] = Patient(patientCount, _ipfsAddress, false);
 		// emit event if needed
 	}
 
