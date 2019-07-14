c1e2e3a14400b2a28d2e1b9e9df44cefc454dd5a
Ahmed Mohamed <amshrbo@gmail.com>
2019-07-14 10:26:58 +0200

    [ADD] (add, get_all, get_all_time_stamp) appointments and docs

--- src/app/api/appointment.py
+++ src/app/api/appointment.py
@@ -30,7 +30,7 @@ def add_appointment():
     update_success = CareBlocks.update_patient_ipfs(address, new_ipfs_hash)
 
     if update_success:
-        return jsonify(status=201)
+        return jsonify(msg='Added succesfully', status=201)
     else:
         return jsonify(msg="Can't add the appointment failed", status=400)
 
@@ -50,7 +50,7 @@ def get_all_appointments():
     all_appointments = IPFS.retrieve_all_appointements(care_blk['ipfs_hash'])
 
     if all_appointments:
-        return jsonify(all_appointments, status=200)
+        return jsonify(all_appointments=all_appointments, status=200)
     else:
         return jsonify(msg="Can't retrieve them", status=400)
 
@@ -64,17 +64,20 @@ def get_all_appointments():
 def get_appointment_ts():
     if not request.is_json:
         return jsonify(msg="Unacceptable data format.", status=406)
-    time_stamp = request.json
+    body = request.json
+
+    if not body['time_stamp']:
+        return jsonify(msg="There isn't a time_stamp filed.", status=406)
 
     current_user = json.loads(get_jwt_identity())
     address = current_user['address']
 
     care_blk = CareBlocks.get_patient(address)
 
-    appointments_after_ts = IPFS.retrieve_appointement_ts(care_blk['ipfs_hash'], time_stamp)
+    appointments_after_ts = IPFS.retrieve_appointement_ts(care_blk['ipfs_hash'], body['time_stamp'])
 
     if appointments_after_ts:
-        return jsonify(appointments_after_ts, status=200)
+        return jsonify(all_appointments=appointments_after_ts, status=200)
     else:
         return jsonify(msg="Can't retrieve them", status=400)
 
