d5447f72245ee5f83930329aa961e2b2f29ea252
hossam <new.hossam0@gmail.com>
2019-07-14 15:14:34 +0200

    [UPDATE] added acc_type to access tokens [UPDATE] restricted doctor creation to orgs [UPDATE] users can list all doctors, orgs list their own [ADD] getting doctor profile

--- src/app/api/organization.py
+++ src/app/api/organization.py
@@ -6,27 +6,36 @@ from utils import general as gutils
 
 
 @app.route('/doctor/register',methods=['POST'])
+@jwt_required
 @swag_from('../docs/swagger/doctor/register.yml')
 def doctor_registeration():
 	if not request.is_json:
 		return jsonify(msg="Not Acceptable data format."), 406
-	mail = request.json.get("email",None)
-	pwd = request.json.get("password",None)
-	if not mail or not pwd:
-		log.error("Registeration - Missing user and password inputs")
-		return jsonify(msg="Invaild credentials!"),400
+	current_user = json.loads(get_jwt_identity())
+	acc_type = current_user["acc_type"]
+	if acc_type == "organization":
+		organization = Organization.objects.get(id=current_user["id"])
+		mail = request.json.get("email",None)
+		pwd = request.json.get("password",None)
+		if not mail or not pwd:
+			log.error("Registeration - Missing user and password inputs")
+			return jsonify(msg="Invaild credentials!"),400
 
-	if len(Doctor.objects.filter(email=mail.lower()) )!=0:
-		log.error("Registeration - already used email - "+mail)
-		return jsonify(msg="This mail can't be used!") ,400
-	doctor = Doctor(email=mail)
-	doctor.password = gutils.hash_SHA512(pwd)
-	doctor.first_name = request.json.get("first_name",None)
-	doctor.last_name = request.json.get("last_name",None)
-	doctor.save()
+		if len(Doctor.objects.filter(email=mail.lower()) )!=0:
+			log.error("Registeration - already used email - "+mail)
+			return jsonify(msg="This mail can't be used!") ,400
+		doctor = Doctor(email=mail)
+		doctor.password = gutils.hash_SHA512(pwd)
+		doctor.first_name = request.json.get("first_name",None)
+		doctor.last_name = request.json.get("last_name",None)
+		doctor.phone_number = request.json.get("phone_number")
+		doctor.organization = organization
+		doctor.save()
 
-	log.info("Registeration Successed - Doctor : " + mail)
-	return jsonify(msg="Registered!"), 201
+		log.info("Registeration Successed - Doctor : " + mail)
+		return jsonify(msg="Registered!"), 201
+	else:
+		return jsonify(msg="Only an organization can register a doctor."), 401
 
 
 
@@ -59,8 +68,14 @@ def orgauth():
 		if not e or len(e)==0 or not gutils.verify_hash(password,e[0].password):
 			return jsonify(msg="Invaild credentials"), 401
 		else:
-			access_token = create_access_token(identity=json.loads(e.to_json()), expires_delta = token_expire)
-			refresh_token = create_refresh_token(identity=json.loads(e.to_json()), expires_delta = refresh_expire)
+			entity = {}
+			e = json.loads(e[0].to_json())
+			entity["id"] = e["_id"]["$oid"]
+			entity["acc_type"] = auth_type
+			entity = json.dumps(entity)
+			print(entity)
+			access_token = create_access_token(identity=entity, expires_delta = token_expire)
+			refresh_token = create_refresh_token(identity=entity, expires_delta = refresh_expire)
 			return jsonify(access_token=access_token,refresh_token=refresh_token, status=200)
 
 
@@ -69,14 +84,34 @@ def orgauth():
 @jwt_required
 @swag_from('../docs/swagger/doctor/list.yml')
 def list_doctors():
-	doctors = json.loads(Doctor.objects.only("first_name","last_name","id").to_json())
-	print(doctors)
-	for doc in doctors:
-		print(doc)
-		doc['id'] = doc['_id']['$oid']
-		doc.pop("_id")
+	current_user = json.loads(get_jwt_identity())
+	if current_user["acc_type"] == "organization":
+		doctors = gutils.get_doctors_list(current_user["id"])
+	elif current_user["acc_type"] == "patient":
+		doctors = gutils.get_doctors_list()
+	else:
+		return jsonify(msg="Unknown requester entity."),401
 	return jsonify(doctors),200
 
+
+@app.route('/doctor/profile',methods=['GET'])
+@jwt_required
+@swag_from('../docs/swagger/doctor/profile.yml')
+def get_doctor_profile():
+	current_user = json.loads(get_jwt_identity())
+	if current_user["acc_type"] == "doctor":
+		doc = Doctor.objects.only("first_name","last_name","email","organization","phone_number").get(id=current_user["id"])
+		print("doc phone = " + doc.phone_number)
+		doctor_obj = {
+		"full_name" : doc.first_name + " " + doc.last_name,
+		"email" : doc.email,
+		"organiztion": doc.organization.full_name,
+		"phone_number": doc.phone_number
+		}
+
+	else:
+		return jsonify(msg="Unauthorized."),401
+	return jsonify(doctor_obj),200
 
 
 @app.route('/organization/register',methods=['POST'])
