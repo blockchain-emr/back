1cb6e31d82fd71de723d1d6949dd360cf70e40f5
Ahmed Mohamed <amshrbo@gmail.com>
2019-07-15 04:34:24 +0200

    adding notification endpoint

---
+++ src/app/api/notification.py
@@ -0,0 +1,23 @@
+import sys, json, time
+sys.path.append("..")
+from utils.careblocks import CareBlocks
+from utils.ipfs import IPFS
+from common.config import *
+
+
+@app.route('/get/notifications', methods=['GET'])
+@jwt.invalid_token_loader
+@jwt_required
+@swag_from('../docs/swagger/notifications/get_notifications.yml')
+def get_notifications():
+    current_user = json.loads(get_jwt_identity())
+    address = current_user['address']
+
+    care_blk = CareBlocks.get_patient(address)
+
+    all_notifications = IPFS.get_notifications(care_blk['ipfs_hash'])
+
+    if all_chronics:
+        return jsonify(all_notifications), 200
+    else:
+        return jsonify(msg="Can't retrieve notifications", status=400)
